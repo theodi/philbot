58fa4212ffc42093381a7e6d70b1bde1db9e513d
James Smith <james@floppy.org.uk>
2013-11-20 14:50:42 +0000

    Correct aruba tmp path

--- features/step_definitions/philbot_steps.rb
+++ features/step_definitions/philbot_steps.rb
@@ -1,9 +1,9 @@
 When(/^the monitor is watching "(.*?)"$/) do |directory|
-  Philbot.run "tmp/aruba/%s" % directory
+  Philbot.run aruba_tmp + '/' + directory
 end
 
 Then(/^the upload of file "(.*?)" should be queued$/) do |filename|
-  Resque.should_receive(:enqueue)#.with(Philbot::Uploader, filename).once
+  Resque.should_receive(:enqueue).with(Philbot::Uploader, [File.expand_path(aruba_tmp + '/' + filename)]).once
 end
 
 When(/^I wait for the monitor to notice$/) do
