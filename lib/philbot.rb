a2637243e05294760047574d04323424dbb71170
pikesley <github@orgraphone.org>
2013-11-20 14:53:50 +0000

    Comment out debug messages

--- lib/philbot.rb
+++ lib/philbot.rb
@@ -6,10 +6,10 @@ require 'listen'
 module Philbot
   def self.run watchdir
     @@listener = Listen.to watchdir do |modified, added, removed|
-      puts "modified absolute path: #{modified}"
-      puts "removed absolute path: #{removed}"
+#      puts "modified absolute path: #{modified}"
+#      puts "removed absolute path: #{removed}"
       if added
-        puts "added absolute path: #{added}"
+#        puts "added absolute path: #{added}"
         Resque.enqueue Philbot::Uploader, added
       end
     end
