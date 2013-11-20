6d87853e66fc67317e4523043b79526285df9baa
pikesley <github@orgraphone.org>
2013-11-20 16:34:47 +0000

    Fake creds

--- lib/philbot/uploader.rb
+++ lib/philbot/uploader.rb
@@ -5,17 +5,16 @@ module Philbot
     @queue = :default
 
     def self.perform filenames
-#     puts "Uploading %s" % filename
       rackspace = Fog::Storage.new(
           {
               provider:           'Rackspace',
               rackspace_username: 'raxdemotheodi', #ENV['RACKSPACE_USERNAME'],
-              rackspace_api_key:  'qwerty', #ENV['RACKSPACE_API_KEY'],
+              rackspace_api_key:  'api_key_of_fake', #ENV['RACKSPACE_API_KEY'],
               rackspace_region:   :lon #ENV['RACKSPACE_REGION'].to_sym
           }
       )
 
-      dir = rackspace.directories.get ENV['RACKSPACE_DB_CONTAINER']
+      dir = rackspace.directories.get 'fake_bucket' # ENV['RACKSPACE_DB_CONTAINER']
       filenames.each do |filename|
         dir.files.create :key => filename, :body => File.open(File.join(Philbot::Config.root, filename))
       end
