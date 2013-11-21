0af3f3357ff1486aaf98a1e7b25b03a09b6b317f
pikesley <github@orgraphone.org>
2013-11-21 12:01:07 +0000

    Empty array considered harmful

--- lib/philbot.rb
+++ lib/philbot.rb
@@ -11,8 +11,16 @@ module Philbot
     Philbot::Config.root = File.expand_path(watchdir)
 
     @@listener = Listen.to watchdir do |modified, added, removed|
-      if added
+      unless added.empty?
+        puts "Added: %s" % added.inspect
+        puts "Queuing %s" % added.inspect
         Resque.enqueue Philbot::Uploader, added.map{ |i| i.gsub('%s/' % watchdir, '')}
+      end
+
+      unless modified.empty?
+        puts "Modified: %s" % modified.inspect
+        puts "Queuing %s" % modified.inspect
+        Resque.enqueue Philbot::Uploader, modified.map{ |i| i.gsub('%s/' % watchdir, '')}
       end
       # we want to handle the other cases, too
     end
