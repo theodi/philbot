8ce091b4e0457061fe4c76bd0ec4c20c94dd45de
pikesley <github@orgraphone.org>
2013-11-22 16:10:36 +0000

    Steps in a step!

--- features/step_definitions/config.steps.rb
+++ features/step_definitions/config.steps.rb
@@ -6,3 +6,17 @@ Then(/^looking up "(.*?)" on the object should yield "(.*?)"$/) do |key, value|
   Philbot::Config.instance[key].should == value
 end
 
+Given(/^a dummy config object$/) do
+  steps %{
+    Given a file named "conf/philbot.yaml" with:
+    """
+    provider:  Rackspace
+    username:  fake_philbot_user
+    api_key:   fake_philbot_key
+    region:    lon
+    container: philbot
+    """
+    When I create a new Philbot::Config object using file "conf/philbot.yaml"
+  }
+end
+
