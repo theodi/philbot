3d1d0633b252efc57dc79be165c35ee2248c5565
pikesley <github@orgraphone.org>
2013-11-22 16:10:26 +0000

    It's a Singleton

--- features/step_definitions/config.steps.rb
+++ features/step_definitions/config.steps.rb
@@ -1,8 +1,8 @@
 When(/^I create a new Philbot::Config object using file "(.*?)"$/) do |yaml|
-  @config = Philbot::Config.new full_path yaml
+  Philbot::Config.instance.configure full_path yaml
 end
 
 Then(/^looking up "(.*?)" on the object should yield "(.*?)"$/) do |key, value|
-  @config[key].should == value
+  Philbot::Config.instance[key].should == value
 end
 
